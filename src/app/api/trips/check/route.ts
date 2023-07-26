8a75be428c677c131335c1bec3e8e743fdb4d6c5
Leonardo Alves <leonardoalves201510@gmail.com>
2023-07-26 09:09:23 -0300

    feat: add revision trip area

--- src/app/api/trips/check/route.ts
+++ src/app/api/trips/check/route.ts
@@ -70,4 +70,14 @@ export async function POST(request: Request) {
       })
     );
   }
+
+  return new NextResponse(
+    JSON.stringify({
+      success: true,
+      trip,
+      totalPrice:
+        differenceInDays(new Date(req.endDate), new Date(req.startDate)) *
+        Number(trip.pricePerDay),
+    })
+  );
 }
--- src/app/trips/[tripId]/confirmation/page.tsx
+++ src/app/trips/[tripId]/confirmation/page.tsx
@@ -3,14 +3,16 @@
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
 import React, { useState, useEffect } from "react";
+import ReactCountryFlag from "react-country-flag";
 
 const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
   const [trip, setTrip] = useState();
+  const [totalPrice, setTotalPrice] = useState<number>(0);
 
   const searchParams = useSearchParams();
   useEffect(() => {
     const fetchTrip = async () => {
-      const response = await fetch(`http://localhost:3000/api/trips//check`, {
+      const response = await fetch(`http://localhost:3000/api/trips/check`, {
         method: "POST",
         body: JSON.stringify({
           tripId: params.tripId,
@@ -19,9 +21,10 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
         }),
       });
 
-      const { trip } = await response.json();
+      const { trip, totalPrice } = await response.json();
 
       setTrip(trip);
+      setTotalPrice(totalPrice);
     };
 
     fetchTrip();
@@ -33,8 +36,40 @@ const TripConfirmation = ({ params }: { params: { tripId: string } }) => {
     <div className="container mx-auto p-5">
       <h1 className="font-semibold text-xl text-primaryDarker">Sua viagem</h1>
 
-      <div className="flex flex-col">
-        <div className="relative h-[106px] w-[124px]"></div>
+      <div className="flex flex-col p-5 mt-5 border-grayLighter border-solid border shadow-lg rounded-lg">
+        <div className="flex items-center gap-4 pb-5 border-b border-grayLighter border-solid">
+          <div className="relative h-[106px] w-[124px]">
+            <Image
+              src={trip.coverImage}
+              fill
+              style={{ objectFit: "cover" }}
+              alt={trip.name}
+              className="rounded-lg"
+            />
+          </div>
+
+          <div className="flex flex-col">
+            <h2 className="text-xl text-primaryDarker font-semibold">
+              {trip.name}
+            </h2>
+
+            <div className="flex items-center gap-1">
+              <ReactCountryFlag countryCode={trip.countryCode} svg />
+              <p className="text-xs text-grayPrimary underline">
+                {trip.location}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <h3 className="font-semibold text-lg text-primaryDarker mt-3">
+          Preço total da viagem
+        </h3>
+
+        <div className="flex justify-between">
+          <p className="font-medium text-primaryDarker">Valor:</p>
+          <p>R${totalPrice}</p>
+        </div>
       </div>
     </div>
   );
