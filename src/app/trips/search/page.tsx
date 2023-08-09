2c981b720a54f9d5e8078adb7ebd0a747ca715aa
Leonardo Alves <leonardoalves201510@gmail.com>
2023-08-09 14:08:14 -0300

    adding search area and showing selected trips

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import TripItem from "@/components/TripItem";
+import { Trip } from "@prisma/client";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect } from "react";
 
 const Trips = () => {
+  const [trips, setTrips] = React.useState<Trip[]>();
+
   const searchParams = useSearchParams();
 
   useEffect(() => {
@@ -18,11 +22,25 @@ const Trips = () => {
 
       const data = await response.json();
 
-      console.log({ data });
+      setTrips(data);
     };
 
     fetchTrips();
   }, []);
 
-  return <div>Trips </div>;
+  return (
+    <div className="container mx-auto flex flex-col items-center p-5">
+      <h1 className="text-primaryDarker font-semibold text-xl">
+        Viagens encontradas
+      </h1>
+      <h2 className="text=grayPrimary font-medium mb-5 ">
+        Listamos as melhores viagens para você!
+      </h2>
+      {trips?.map((trip) => (
+        <TripItem key={trip.id} trip={trip} />
+      ))}
+    </div>
+  );
 };
+
+export default Trips;
