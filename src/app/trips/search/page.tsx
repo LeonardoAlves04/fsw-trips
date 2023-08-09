07fd72477397d8ca480bf13e42fedbfa93d9d94e
Leonardo Alves <leonardoalves201510@gmail.com>
2023-08-09 14:16:55 -0300

    feat: add trip search

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -33,12 +33,16 @@ const Trips = () => {
       <h1 className="text-primaryDarker font-semibold text-xl">
         Viagens encontradas
       </h1>
-      <h2 className="text=grayPrimary font-medium mb-5 ">
-        Listamos as melhores viagens para você!
+      <h2 className="text=grayPrimary font-medium mb-5 mt-2">
+        {trips?.length > 0
+          ? "Listamos as melhores viagens para você!"
+          : "Não encontramos essa viagem :("}
       </h2>
-      {trips?.map((trip) => (
-        <TripItem key={trip.id} trip={trip} />
-      ))}
+      <div className=" flex flex-col gap-5">
+        {trips?.map((trip) => (
+          <TripItem key={trip.id} trip={trip} />
+        ))}
+      </div>
     </div>
   );
 };
