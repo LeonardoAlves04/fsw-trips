1c3e01cc65e90ec4bd9f695453532df5f9041116
Leonardo Alves <leonardoalves201510@gmail.com>
2023-08-14 16:17:42 -0300

    style: adapt searchpage to desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -29,16 +29,16 @@ const Trips = () => {
   }, []);
 
   return (
-    <div className="container mx-auto flex flex-col items-center p-5">
-      <h1 className="text-primaryDarker font-semibold text-xl">
+    <div className="container mx-auto flex flex-col items-center p-5 lg:items-center">
+      <h1 className="text-primaryDarker font-semibold text-xl text-center lg:w-full lg:text-[2.5rem]">
         Viagens encontradas
       </h1>
-      <h2 className="text=grayPrimary font-medium mb-5 ">
+      <h2 className="text=grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-center ">
         {trips.length > 0
           ? "Listamos as melhores viagens para você!"
           : "Não encontramos essa viagem :("}
       </h2>
-      <div className=" flex flex-col gap-5">
+      <div className=" flex flex-col gap-5 lg:grid lg:grid-cols-4 lg:gap-10	lg:mt-6 lg:pb-16">
         {trips?.map((trip) => (
           <TripItem key={trip.id} trip={trip} />
         ))}
